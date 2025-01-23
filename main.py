adec404b3189b77c8a64dba8417419e96564d825
Yongqi Huang <yqhuang@tju.edu.cn>
2025-01-23 11:39:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,10 +22,10 @@ with open("README.md", "r") as f:
     # if last_update_date == current_date:
         # sys.exit("Already updated today!")
 
-keywords = ["Time Series", "Trajectory", "Graph Neural Networks"] # TODO add more keywords
+keywords = ["Graph Neural Networks"] # TODO add more keywords
 
 max_result = 100 # maximum query results from arXiv API for each keyword
-issues_result = 15 # maximum papers to be included in the issue
+issues_result = 10 # maximum papers to be included in the issue
 
 # all columns: Title, Authors, Abstract, Link, Tags, Comment, Date
 # fixed_columns = ["Title", "Link", "Date"]
